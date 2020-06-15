301d8c1a2521f10d43ba8e84fe697602f8529e01
17jmalek98 <17jmalek98@gmail.com>
2020-06-15 15:59:23 -0700

    added listifier

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           title: Text('SUiCID.io'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
@@ -32,8 +32,37 @@ class RandomWords extends StatefulWidget{
 class RandomWordsState extends State<RandomWords>{
   //add build method
   @override 
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+  return ListView.builder(
+    padding: const EdgeInsets.all(16.0),
+    itemBuilder: /*1*/ (context, i) {
+      if (i.isOdd) return Divider(); /*2*/
+
+      final index = i ~/ 2; /*3*/
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+  }
+
   Widget build(BuildContext context){
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
