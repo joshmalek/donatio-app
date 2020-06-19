0e647d6a2c8f66ee8972b79c3e7164a780f4e475
17jmalek98 <17jmalek98@gmail.com>
2020-06-18 19:30:37 -0700

    dropdown add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 
@@ -65,7 +66,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Http App',
       theme: ThemeData(
-          scaffoldBackgroundColor: Colors.black,
+          scaffoldBackgroundColor: Color(0xff1A191A),
           textTheme: Theme.of(context).textTheme.apply(
                 bodyColor: Colors.white,
                 displayColor: Colors.white,
@@ -106,6 +107,7 @@ class _LeaderboardState extends State {
 
   @override
   build(context) {
+    String dropdownValue = 'All Time';
     return Scaffold(
         body: Column(children: <Widget>[
       SizedBox(height: 30),
@@ -114,46 +116,79 @@ class _LeaderboardState extends State {
           SizedBox(
             width: 10,
           ),
-          Icon(Icons.accessible_forward, color: Colors.white),
+          Container(
+            height: 25,
+            width: 25,
+            child: SvgPicture.asset('assets/menu.svg', color: Colors.white),
+          ),
           SizedBox(
             width: 20,
           ),
           Text("Leaderboard", style: GoogleFonts.workSans(fontSize: 20))
         ],
       ),
-      ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(8),
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          return Card(
-              color: index == 0
-                  ? Colors.blue
-                  : index == 1
-                      ? Colors.amber
-                      : index == 2
-                          ? Colors.blueGrey
-                          : index == 3 ? Colors.brown : Colors.deepPurple,
-              shadowColor: Colors.red,
-              child: ListTile(
-                  leading: Text((index + 1).toString(),
-                      style: GoogleFonts.workSans(fontSize: 20)),
-                  title: Text(users[index].name, style: GoogleFonts.workSans()),
-                  trailing: Text(users[index].xp.toString(),
-                      style: GoogleFonts.workSans(fontSize: 20)),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => UserDetails(user: users[index]),
-                      ),
-                    );
-                  }
-                  //subtitle: Text("XP: " + users[index].xp.toString()),
-                  ));
+      DropdownButton<String>(
+        value: dropdownValue,
+        dropdownColor: Colors.blueGrey,
+        icon: Icon(Icons.keyboard_arrow_down),
+        onChanged: (String newValue) {
+          setState(() {
+            dropdownValue = newValue;
+          });
         },
-      )
+        items: <String>['All Time', 'Two', 'Free', 'Four']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
+      Container(
+          height: 600,
+          child: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(height: 14),
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(15),
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  color: index == 0
+                      ? Colors.blue
+                      : index == 1
+                          ? Colors.amber
+                          : index == 2
+                              ? Colors.blueGrey
+                              : index == 3 ? Colors.brown : Colors.deepPurple,
+                  shadowColor: Colors.red,
+                  child: ListTile(
+                      dense: true,
+                      leading: Text((index + 1).toString(),
+                          style: GoogleFonts.workSans(fontSize: 20)),
+                      title: Text(users[index].name,
+                          style: GoogleFonts.workSans(fontSize: 18)),
+                      trailing: Text(users[index].xp.toString(),
+                          style: GoogleFonts.workSans(fontSize: 20)),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                UserDetails(user: users[index]),
+                          ),
+                        );
+                      }
+                      //subtitle: Text("XP: " + users[index].xp.toString()),
+                      ));
+            },
+          )),
+      Container(
+        height: 75,
+        width: 75,
+        child: SvgPicture.asset('assets/logo.svg'),
+      ),
     ]));
   }
 }
