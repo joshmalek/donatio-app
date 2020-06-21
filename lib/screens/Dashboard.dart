5033f2b1b861d6cbd2ee16f2581ffc09c7c8284d
Muiz Yusuff <muiz.project@gmail.com>
2020-06-21 17:18:08 -0400

    Added new logo to project

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -9,69 +9,69 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavbar(),
-        body: Text("Updated Dashboard"));
+        bottomNavigationBar: BottomNavbar(0), body: Text("Updated Dashboard"));
   }
 }
 
 class BottomNavbar extends StatelessWidget {
+  int _pageIndex = 0;
+  BottomNavbar(this._pageIndex);
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Container(
-            height: 40.0,
-            margin: EdgeInsets.fromLTRB(0, 0, 0, 25),
-            child: Container(
-              margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
-              width: 150.0,
-              child: Stack(
+        height: 40.0,
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 25),
+        child: Container(
+          margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+          width: 150.0,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                  child: Container(
+                      width: (2 *
+                              ((MediaQuery.of(context).size.width - 80) / 11)) -
+                          10,
+                      height: 40,
+                      color: Colors.orange),
+                  left: 5),
+              Row(
                 children: <Widget>[
-                  Positioned(
+                  Flexible(
+                      flex: 2,
                       child: Container(
-                          width: (2 *
-                                  ((MediaQuery.of(context).size.width - 80) /
-                                      11)) -
-                              10,
-                          height: 40,
-                          color: Colors.orange),
-                      left: 5),
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                          flex: 2,
-                          child: Container(
-                              alignment: Alignment.center,
-                              child: Icon(Icomoon.dashboard, size: 17))),
-                      Flexible(
-                          flex: 2,
-                          child: Container(
-                              alignment: Alignment.center,
-                              child: Icon(Icomoon.hexagon2, size: 22))),
-                      Flexible(
-                          flex: 3,
-                          child: Container(
-                              child: SvgPicture.asset(
-                                "assets/logo_2.svg",
-                                semanticsLabel: "DonatIO Logo",
-                              ),
-                              height: 60,
-                              alignment: Alignment.center)),
-                      Flexible(
-                          flex: 2,
-                          child: Container(
-                              alignment: Alignment.center,
-                              child: Icon(Icomoon.trophy, size: 20))),
-                      Flexible(
-                          flex: 2,
-                          child: Container(
-                              alignment: Alignment.center,
-                              child: Icon(Icomoon.coin, size: 18)))
-                    ],
-                  )
+                          alignment: Alignment.center,
+                          child: Icon(Icomoon.dashboard, size: 17))),
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: Icon(Icomoon.hexagon2, size: 22))),
+                  Flexible(
+                      flex: 3,
+                      child: Container(
+                          child: SvgPicture.asset(
+                            "assets/logo_4.svg",
+                            semanticsLabel: "DonatIO Logo",
+                          ),
+                          height: 60,
+                          alignment: Alignment.center)),
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: Icon(Icomoon.trophy, size: 20))),
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: Icon(Icomoon.coin, size: 18)))
                 ],
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(5)),
-            ))
+              )
+            ],
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(5)),
+        ));
   }
 }
