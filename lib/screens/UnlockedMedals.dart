99732005afb6a13be0e53483d6ddbf136e7cd429
Muiz Yusuff <muiz.project@gmail.com>
2020-06-22 18:27:51 -0400

    Added medals header to medals view

--- lib/screens/UnlockedMedals.dart
+++ lib/screens/UnlockedMedals.dart
@@ -1,6 +1,9 @@
 import 'package:donatio_app/components/Navbar.dart';
+import 'package:donatio_app/src/ThemePalette.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'Dashboard.dart';
 
 class UnlockedMedalsScreen extends StatelessWidget {
   @override
@@ -17,7 +20,30 @@ class UnlockedMedalsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text("Unlocked Medals Screen Placeholder"),
+      child: Column(
+        children: <Widget>[AppHeader(), MedalsHeader()],
+      ),
     );
+  }
+}
+
+class MedalsHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.centerLeft,
+        margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text("Unlocked Medals", style: FontPresets.title1),
+              alignment: Alignment.centerLeft,
+            ),
+            Container(
+              child: Text("You unlocked 5 medals", style: FontPresets.label),
+              alignment: Alignment.centerLeft,
+            )
+          ],
+        ));
   }
 }
