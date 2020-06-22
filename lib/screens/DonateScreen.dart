9cb93c5653e1fe2c44acbf5d6201081efef98e47
Muiz Yusuff <muiz.project@gmail.com>
2020-06-22 16:52:36 -0400

    Began donate tab

--- lib/screens/DonateScreen.dart
+++ lib/screens/DonateScreen.dart
@@ -1,4 +1,6 @@
 import 'package:donatio_app/components/Navbar.dart';
+import 'package:donatio_app/screens/Dashboard.dart';
+import 'package:donatio_app/src/ThemePalette.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +19,34 @@ class DonateBody extends StatelessWidget {
   @override
   Widget build(BuildContext conext) {
     return SafeArea(
-      child: Text("Donate Screen Placeholder"),
-    );
+        child: Column(
+      children: <Widget>[AppHeader(), DonateHeader()],
+    ));
+  }
+}
+
+class DonateHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Container(
+            alignment: Alignment.centerLeft,
+            child: Text("Donate", style: FontPresets.title1),
+            margin: EdgeInsets.fromLTRB(50, 10, 50, 0)),
+        Container(
+            margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+            alignment: Alignment.centerLeft,
+            child: Text("Want to donate to charity?"))
+      ],
+    ));
+  }
+}
+
+class DonateParagraph extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
