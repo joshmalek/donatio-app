f256ce91ca0d7e87ee6011768284c57bae3b8ec7
17jmalek98 <17jmalek98@gmail.com>
2020-06-22 23:33:18 -0700

    updated leaderboard to pull api

--- lib/models/API.dart
+++ lib/models/API.dart
@@ -1,11 +1,12 @@
 import 'package:http/http.dart' as http;
 
-const baseUrl = 'http://3.21.56.172:4000/graphql?query=';
+const baseUrl =
+    'http://10.0.2.2:4000/graphql?query={users{firstName,lastName,email,_id,experience,total_donated,medals{name,description,img_url}}}';
 
 class API {
   static Future getUsers() {
+    print(baseUrl);
     //var url = baseUrl + "{users{id,firstName,lastName,email}}";
-    return http.get(baseUrl +
-        '{users{firstName,lastName,email,_id,experience,total_donated,medals{name,description,img_url}}}');
+    return http.get(baseUrl);
   }
 }
